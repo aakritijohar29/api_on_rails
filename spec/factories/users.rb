dd1c7237d795add6df93fd150938f9fe582c040d
Bruno Paulino <brunojppb@gmail.com>
2015-03-24 21:22:59 -0400

    Adds User model firsts specs

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,10 @@
+require 'ffaker'
+
 FactoryGirl.define do
   factory :user do
-    
+    email { FFaker::Internet.email }
+    password "12345678"
+    password_confirmation "12345678"
   end
 
 end
