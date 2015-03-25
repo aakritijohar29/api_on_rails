3c8db1c43e65d5c8d4b84937b807ffaa91ec3908
Bruno Paulino <brunojppb@gmail.com>
2015-03-24 22:43:05 -0400

    Adds #create and #update actions to the User Controller

---
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -0,0 +1,138 @@
+require 'rails_helper'
+require 'spec_helper'
+
+RSpec.describe Api::V1::UsersController, type: :controller do
+
+  before(:each) { request.headers['Accept'] = "application/vnd.marketplace.v1" }
+
+  describe "GET #show" do
+    # As a developer using RSpec
+    # I want to execute arbitrary code before and after each example
+    # So that I can control the environment in which it is run
+    #RSPec documentation: https://www.relishapp.com/rspec/rspec-core/v/2-0/docs/hooks/before-and-after-hooks
+    before(:each) do
+      @user = FactoryGirl.create :user
+      get :show, id: @user.id, format: :json  
+    end
+
+    it "returns the information about a reporter on a hash" do
+      user_response = JSON.parse(response.body, symbolize_name: true)
+      expect(user_response['email']).to eql @user.email
+    end
+
+    #GET requests generally return 200 code (The request has succeeded)
+    it { should respond_with 200 }
+  end
+
+  describe "POST #create" do
+
+    #==========================================================================
+    context "when is successfully created" do
+      before(:each) do
+        @user_attibutes = FactoryGirl.attributes_for :user
+        post :create, { user: @user_attibutes }, format: :json
+      end
+
+      it "renders the JSON representation for the user record just created" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response['email']).to eql @user_attibutes['email']
+      end
+
+      #POST requests 201 means the request has been fulfilled 
+      #and resulted in a new resource being created.
+      it { should respond_with 201 }
+    end
+
+    #==========================================================================
+    context "when is not created" do
+      before(:each) do
+        #I am not including the User's email
+        @invalid_user_attributes = { password: "12345678", password_confirmation: "12345678" }
+        post :create, { user: @invalid_user_attributes }, format: :json
+      end
+
+      it "renders an json error" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response).to have_key(:errors)
+      end
+
+      it "renders the JSON error explaning why the user could not be created" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:errors][:email]).to include "can't be blank"
+      end
+
+      #POST requests usually returns 422 code in case of errors
+      it { should respond_with 422 }
+    end
+
+  end
+
+  describe "PUT/PATCH #update" do
+
+    #=============================================================
+    context "when is successfully updated" do 
+      before(:each) do
+        @user = FactoryGirl.create :user
+        patch :update, { id: @user.id, user: { email: "newemail@example.com" } }, format: :json
+      end
+
+      it "renders the JSON representation for the updated User" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:email]).to eql "newemail@example.com"
+      end
+
+      # 200 code == OK
+      it { should respond_with 200 }
+    end
+
+    context "when is not updated" do
+
+      before(:each) do
+        @user = FactoryGirl.create :user
+        patch :update, { id: @user.id, user: { email: "invalid.com" } }, format: :json
+      end
+
+      it "renders and JSON error" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response).to have_key(:errors)
+      end
+
+      it "renders the JSON error explaning why the user could not be updated" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:errors][:email]).to include "is invalid"
+      end
+
+      # 422 code == The request was well-formed but was unable to be followed due to semantic errors
+      it { should respond_with 422 }
+
+    end
+
+  end
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
