d4a59a4728ea9892ed3d15e76c9343411a097594
Bruno Paulino <brunojppb@gmail.com>
2015-03-27 13:15:25 -0400

    Adds Product validations

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   
   let(:product) { FactoryGirl.build :product }
+  #subject(:product) { product }
 
   it { should respond_to(:title) }
   it { should respond_to(:price) }
@@ -10,5 +11,10 @@ RSpec.describe Product, type: :model do
   it { should respond_to(:user_id) }
 
   it { should_not be_published }
+
+  it { should validate_presence_of :title }
+  it { should validate_presence_of :price }
+  it { should validate_numericality_of(:price).is_greater_than_or_equal_to(0) }
+  it { should validate_presence_of :price }
 
 end
