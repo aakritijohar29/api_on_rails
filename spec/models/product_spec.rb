ea68c8d77b421e041e2919c10fae6775fb2f899f
Bruno Paulino <brunojppb@gmail.com>
2015-03-26 21:53:38 -0400

    Product model and attributes tests

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  
+  let(:product) { FactoryGirl.build :product }
+
+  it { should respond_to(:title) }
+  it { should respond_to(:price) }
+  it { should respond_to(:published) }
+  it { should respond_to(:user_id) }
+
+  it { should_not be_published }
+
+end
