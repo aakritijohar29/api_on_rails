87f499da2a26fd2dda97fccc45878f59007435d6
Bruno Paulino <brunojppb@gmail.com>
2015-03-29 13:15:41 -0400

    fix .recent scope for Product Model

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,6 +8,17 @@ class Product < ActiveRecord::Base
   scope :filter_by_title, lambda { |keyword| where("lower(title) LIKE ?", "%#{keyword.downcase}%") }
   scope :above_or_equal_to_price, lambda { |price| where("price >= ?", price) }
   scope :below_or_equal_to_price, lambda { |price| where("price <= ?", price) }
-  scope :recent, lambda { order(:updated_at) }
+  scope :recent, lambda { order("products.updated_at DESC") }
+
+  def self.search(params = {})
+    products = params[:product_ids].present? ? Product.find(params[:product_ids]) : Product.all
+
+    products = products.filter_by_title(params[:keyword]) if params[:keyword]
+    products = products.above_or_equal_to_price(params[:min_price]) if params[:min_price]
+    products = products.below_or_equal_to_price(params[:max_price]) if params[:max_price]
+    products = products.recent() if params[:recent]
+
+    products
+  end
 
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -80,6 +80,43 @@ RSpec.describe Product, type: :model do
     it "returns the most updated records" do
       expect(Product.recent).to match_array([@product3, @product2, @product4, @product1])
     end
+  end
+
+  describe "search" do
+    before(:each) do
+      @product1 = FactoryGirl.create :product, price: 100,  title: "Ford Focus"
+      @product2 = FactoryGirl.create :product, price: 50,   title: "Tesla model S"
+      @product3 = FactoryGirl.create :product, price: 150,  title: "Toyota Prius"
+      @product4 = FactoryGirl.create :product, price: 99,   title: "Tesla model X"
+    end
+
+    context "when title 'tesla' and '120' a min price are set" do
+      
+      it "returns an empty array" do
+        search_hash = { keyword: "tesla", min_price: 120 }
+        expect(Product.search(search_hash)).to be_empty
+      end
+    end
+
+    context "when title 'ford', '150' as max price and '50' as min price are set" do
+      it "returns the product1" do
+        search_hash = { keyword: "ford", max_price: 150, min_price: 50 }
+        expect(Product.search(search_hash)).to match_array([@product1])
+      end
+    end
+
+    context "when a empty hash is sent" do
+      it "returns all the products" do
+        expect(Product.search({})).to match_array([@product1, @product2, @product3, @product4])
+      end
+    end
+
+    context "when product_ids is present" do
+      it "returns the product from the ids" do
+        search_hash = { product_ids: [@product1.id, @product2.id] }
+        expect(Product.search(search_hash)).to match_array([@product1, @product2])
+      end
+    end
 
   end
 
