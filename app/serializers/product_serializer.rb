575caa0c9af7e2e7d7614c5c09c80ddb3fc75ba1
Bruno Paulino <brunojppb@gmail.com>
2015-03-30 00:46:10 -0400

    Adds caching for product serializer

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,5 +1,10 @@
 class ProductSerializer < ActiveModel::Serializer
+  cached
+
   attributes :id, :title, :price, :published
- 
   has_one :user, include: true
+
+  def cache_key
+    [object, scope]
+  end
 end
